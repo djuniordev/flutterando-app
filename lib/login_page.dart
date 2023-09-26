35e159cd5e75c779ef206c581f38daac12e22890
Daniel Júnior <danieljunior.me@gmail.com>
2023-09-26 16:02:13 -0300

    feat: Colocando imagem no aplicativo e alterando a cor do botão e tamanho

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,7 +14,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView( //Scrow na tela toda
+      body: SingleChildScrollView(
+        //Scrow na tela toda
         child: SizedBox(
           width: MediaQuery.of(context).size.width, //Pegar largura automatico
           height: MediaQuery.of(context).size.height, //Pegar altura automatico
@@ -22,11 +23,21 @@ class _LoginPageState extends State<LoginPage> {
             padding: const EdgeInsets.all(8.0), //Espaço nas bordas
             child:
                 Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                height: 100,
+                width: 100,
+                child: Image.asset("assets/images/logoflutter.png"),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              //Image.network("https://w7.pngwing.com/pngs/537/866/png-transparent-flutter-hd-logo.png")), colocar imagem da internet
               TextField(
                 onChanged: (text) {
                   email = text;
                 },
-                keyboardType: TextInputType.emailAddress, //Aparecer o @ no teclado do usuario
+                keyboardType: TextInputType
+                    .emailAddress, //Aparecer o @ no teclado do usuario
                 decoration: InputDecoration(
                     labelText: "Email", border: OutlineInputBorder()),
               ),
@@ -38,21 +49,30 @@ class _LoginPageState extends State<LoginPage> {
                   senha = text;
                 },
                 obscureText: true, //Deixar a senha em *
-                decoration: InputDecoration( //Decorar o input
-                    labelText: "Senha", border: OutlineInputBorder()), //Deixar borda arredondada
+                decoration: InputDecoration(
+                    //Decorar o input
+                    labelText: "Senha",
+                    border: OutlineInputBorder()), //Deixar borda arredondada
               ),
-              SizedBox( //Dar um Espaço
+              SizedBox(
+                //Dar um Espaço
                 height: 15,
               ),
-              ElevatedButton( //Botao
-                  onPressed: () {
-                    if (email == "teste@gmail.com" && senha == "123") {
-                      Navigator.of(context).pushReplacementNamed('/home'); //Navegadar pelo route
-                    } else {
-                      print("Login inválido");
-                    }
-                  },
-                  child: Text('Entrar')),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  minimumSize: Size(150, 60),
+                ), //Botao
+                onPressed: () {
+                  if (email == "123" && senha == "123") {
+                    Navigator.of(context)
+                        .pushReplacementNamed('/home'); //Navegadar pelo route
+                  } else {
+                    print("Login inválido");
+                  }
+                },
+                child: Text('Entrar'),
+              ),
             ]),
           ),
         ),
